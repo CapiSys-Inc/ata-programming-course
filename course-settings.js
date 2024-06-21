ed95f3e052acf1441db9d7c7ae24719b7889e6c9
Arrow-Holdings <rick@nexopay.es>
2024-06-21 10:36:44 +0700

    first commit

---
+++ course-settings.js
@@ -0,0 +1,23 @@
+const courseSettings = {
+  language: "en",
+  name: "ATA Python Programming Course",
+  siteUrl: "https://programming-24.mooc.fi",
+  githubUrl: "https://github.com/rage/programming-24",
+  subtitle: "ATA Python Programming Course",
+  slug: "programming-24",
+  organizationName: "MOOC",
+  tmcCourse: "programming-24",
+  quizzesId: "0a8162e8-133a-4fa8-b619-4125cb92c6e0",
+  tmcOrganization: "mooc",
+  bannerPath: "banner.svg",
+  showExerciseDescriptionWhenNotLoggedIn: true,
+  courseVariants: [
+    // {
+    //   tmcOrganization: "hbc",
+    //   tmcCourse: "python-TVT21A",
+    //   // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
+    // },
+  ],
+}
+
+module.exports = courseSettings
