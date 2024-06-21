ff3103d1451789ec9757cb03e7f588577682d28f
Arrow-Holdings <rick@nexopay.es>
2024-06-21 12:13:19 +0700

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,6 @@ module.exports = {
       options: {
         path: `${__dirname}/data`,
         name: "markdown-pages",
-
       }
     },
     `gatsby-plugin-anchor-links`,
@@ -46,43 +45,16 @@ module.exports = {
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
-              // Class prefix for <pre> tags containing syntax highlighting;
-              // defaults to 'language-' (eg <pre class="language-js">).
-              // If your site loads Prism into the browser at runtime,
-              // (eg for use with libraries like react-live),
-              // you may use this to prevent Prism from re-processing syntax.
-              // This is an uncommon use-case though;
-              // If you're unsure, it's best to use the default value.
               classPrefix: "language-",
-              // This is used to allow setting a language for inline code
-              // (i.e. single backticks) by creating a separator.
-              // This separator is a string and will do no white-space
-              // stripping.
-              // A suggested value for English speakers is the non-ascii
-              // character '›'.
               inlineCodeMarker: null,
-              // This lets you set up language aliases.  For example,
-              // setting this to '{ sh: "bash" }' will let you use
-              // the language "sh" which will highlight using the
-              // bash highlighter.
               aliases: {},
-              // This toggles the display of line numbers alongside the code.
-              // To use it, add the following line in src/layouts/index.js
-              // right after importing the prism color scheme:
-              //  `require("prismjs/plugins/line-numbers/prism-line-numbers.css");`
-              // Defaults to false.
               showLineNumbers: false,
-              // If setting this to true, the parser won't handle and highlight inline
-              // code used in markdown i.e. single backtick code like `this`.
               noInlineHighlight: false
             }
           },
           {
             resolve: `gatsby-remark-images`,
             options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
               maxWidth: 920,
               withWebp: true,
               wrapperStyle: "margin-bottom: 1rem;",
@@ -99,13 +71,9 @@ module.exports = {
           {
             resolve: "gatsby-remark-emojis",
             options: {
-              // Deactivate the plugin globally (default: true)
               active: true,
-              // Add a custom css class
               class: "emoji-icon",
-              // Select the size (available size: 16, 24, 32, 64)
               size: 64,
-              // Add custom styles
               styles: {
                 display: "inline",
                 margin: "0",
@@ -124,9 +92,6 @@ module.exports = {
       resolve: `gatsby-plugin-sitemap`,
       options: {
         output: `/sitemap.xml`,
-        // Exclude specific pages or groups of pages using glob parameters
-        // See: https://github.com/isaacs/minimatch
-        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
         exclude: ["/missing-info", "/profile", "/sign-in", "/sign-out", "/sign-up", "/404"],
         query: `
         {
@@ -155,6 +120,18 @@ module.exports = {
     `gatsby-plugin-top-layout`,
     {
       resolve: 'gatsby-plugin-material-ui',
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: CourseSettings.name,
+        short_name: CourseSettings.name,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
+        display: `minimal-ui`,
+        icon: `static/favicon.png`, // This path is relative to the root of the site.
+      },
     },
     `gatsby-plugin-meta-redirect` // make sure to put last in the array
   ]
